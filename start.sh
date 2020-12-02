bb57c4357bed471dd56e0045e70f5d577a0cf5e5
Ruben Ollesch <ruben.ollesch@studmail.w-hs.de>
2020-12-02 20:08:52 +0100

    Comment start script

--- start.sh
+++ start.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-ram=4G
+
+# set working directory to script directory
+cd "${0%/*}"
 
 
-java -Xms${ram} -Xmx${ram} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar nogui
+
+# Java uses additional memory on top of this value
+RAM=10G
+
+
+# Using aikar's flags
+# https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/
+
+java -Xms${RAM} -Xmx${RAM} -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+	-XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions \
+	-XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 \
+	-XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M \
+	-XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 \
+	-XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 \
+	-XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 \
+	-XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
+	-Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true \
+	-jar paper.jar nogui
