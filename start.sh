f96c33da246349ed92fc26e8fd3bd33297bd8e1d
Ruben Ollesch <ruben.ollesch@protonmail.com>
2020-12-14 10:38:25 +0100

    Adjust java parameters for large amounts of memory

--- start.sh
+++ start.sh
@@ -7,18 +7,35 @@ cd "${0%/*}"
 
 
 # Java uses additional memory on top of this value
-RAM=32G
+gigs=32
 
+
+
+newsizepercent=30
+maxnewsizepercent=40
+heapregionsize=8M
+reservepercent=20
+initiatingheapoccupancypercent=15
+
+# Adjust parameters for large amounts of memory
+if [ "${gigs}" -gt 12 ]; then
+	newsizepercent=40
+	maxnewsizepercent=50
+	heapregionsize=16M
+	reservepercent=15
+	initiatingheapoccupancypercent=20
+fi
 
 # Using aikar's flags
 # https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/
 
-java -Xms${RAM} -Xmx${RAM} -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+java -Xms${gigs}G -Xmx${gigs}G -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
 	-XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions \
-	-XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 \
-	-XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M \
-	-XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 \
-	-XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 \
+	-XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
+	-XX:G1NewSizePercent=${newsizepercent} -XX:G1MaxNewSizePercent=${maxnewsizepercent} \
+	-XX:G1HeapRegionSize=${heapregionsize} -XX:G1ReservePercent=${reservepercent} \
+	-XX:InitiatingHeapOccupancyPercent=${initiatingheapoccupancypercent} \
+	-XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 \
 	-XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 \
 	-XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
 	-Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true \
